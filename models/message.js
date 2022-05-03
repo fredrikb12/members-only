5f0479f1bb3f5144a691d60d00c14d485a97f5d1
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-03 10:37:37 +0200

    Create message model

---
+++ models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, required: true },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
